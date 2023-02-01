9f3b4f8c6c9641b8e2ab9b593c6a4ad5c9e5cefa
Khalid Momand <mohammadkhalidmomand@gmail.com>
2023-02-01 21:03:55 +0400

    new changes done for the localhost

--- frontend/src/assets/api/api.jsx
+++ frontend/src/assets/api/api.jsx
@@ -2,5 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'https://mkm-voter.onrender.com/',
   });
+
+  // http://localhost:8000/
